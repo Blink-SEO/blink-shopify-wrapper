cfd81dd155646a6af45ad9be9dbcf54d0121d2aa
Steven Bister <stevebister@gmail.com>
2022-02-07 10:03:26 +0000

    Update to use Shopify CLI (#1)

---
+++ assets/blink-scripts.js
@@ -0,0 +1,11 @@
+/**
+ * Wrap your scripts within an IIFE or a DOMContentLoaded eventlistener.
+ *
+ * This will scope our scripts to prevent potential conflicts between
+ * our custom work and the main theme stylesheet.
+ */
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Custom scripts go here...
+  console.log('document ready'); // delete me
+});
