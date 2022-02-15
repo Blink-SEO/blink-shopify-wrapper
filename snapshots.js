530826432a6909c729b8b7294e43174e491a134a
Steven Bister <stevebister@gmail.com>
2022-02-15 13:38:56 +0000

    Refactor: Removed .env file and moved all config settings within the snapshots.js file

--- snapshots.js
+++ snapshots.js
@@ -1,41 +1,50 @@
-require('dotenv').config();
-
-// List of pages we want to test
-// Name is optional as we will fallback to the url if it is not present.
-// We can also pass other options to each object if we want them to run on specific pages
-// see https://docs.percy.io/docs/cli-snapshot#usage for examples
-const pagesConfig = [
-  { name: 'Home', url: '/' },
-  {
-    name: 'About',
-    url: '/pages/about-us/',
-  },
-];
+/**
+ * Snapshot config
+ *
+ * List of pages we want to test
+ * Name is optional as we will fallback to the url if it is not present.
+ * We can also pass other options to each object if we want them to run on specific pages
+ * see https://docs.percy.io/docs/cli-snapshot#usage for examples.
+ */
+const snapshotConfig = {
+  store: 'https://blink-developer.myshopify.com', // Update this to the store you are working on
+  previewID: 125258629288, // Update this to the theme ID you are working on
+  devMode: false,
+  pages: [
+    // Example pages make sure to update these
+    { name: 'Home', url: '/' },
+    {
+      name: 'About',
+      url: '/pages/about-us/',
+    },
+  ],
+};
 
 /**
  * Create our snapshot urls list
  *
+ * @param {object} - config object for our snapshot to run
+ * @returns {array} - array of objects containing our page names and urls
  * @link https://docs.percy.io/docs/cli-snapshot
- * @returns {array} array of objects containing our page names and urls
  */
-const createSnapshotUrls = (pages) => {
-  const store = process.env.STORE_URL || '';
-  const previewID = process.env.PREVIEW_ID
-    ? `?preview_theme_id=${process.env.PREVIEW_ID}`
+const createSnapshotUrls = (config) => {
+  const store = config.store || '';
+  const previewID = config.previewID
+    ? `?preview_theme_id=${config.previewID}`
     : '';
 
   // Attempt to warn user that they haven't changed the default settings
-  if (!process.env.LOCAL_MODE && store.includes('blink-developer')) {
+  if (config.devMode === false && store.includes('blink-developer')) {
     console.warn(
-      'It looks like you are trying to run this on the blink-development store. Make sure to change your .env file to update the url. \nAlternatively add LOCAL_MODE=true to your .env file',
+      'It looks like you are trying to run this on the blink-development store. Make sure to change your .env file to update the url. \nAlternatively change snapshotConfig.devMode to true.',
     );
     return;
   }
 
-  return pages.map((page) => ({
+  return config.pages.map((page) => ({
     name: page.name || page.url,
     url: `${store}${page.url}${previewID}`,
   }));
 };
 
-module.exports = createSnapshotUrls(pagesConfig);
+module.exports = createSnapshotUrls(snapshotConfig);
